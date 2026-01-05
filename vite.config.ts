f702988cd2ad1687951742642eae4b3d21f20eb7
Laurent Mwamba <lmwamba@netflix.com>
2026-01-05 02:46:29 -0600

    fixing CommonJS deprecation warning by adding "type": "module" to package.json and replacing CommonJS variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -17,7 +18,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': fileURLToPath(new URL('.', import.meta.url)),
       },
     },
     test: {
